72850e31cc87ab490e9d29fbf2942030cd0a4e99
krishna sandeep <vanamasandeep2019@gmail.com>
2020-05-25 11:29:31 +0530

    Java program to reverse elements in an array in java

---
+++ src/arrays/ReverseArray.java
@@ -0,0 +1,25 @@
+package arrays;
+
+public class ReverseArray {
+	
+	public static void main(String[] args) {
+		
+		int arr[]={10,20,30,40,50};
+		
+		int low=0,high=arr.length-1;
+		
+		while(low<high){
+			int temp=arr[low];
+			arr[low]=arr[high];
+			arr[high]=temp;
+			low++;
+			high--;
+		}
+		
+		for (int i : arr) {
+			System.out.println(i);
+		}
+	}
+	
+	
+}
